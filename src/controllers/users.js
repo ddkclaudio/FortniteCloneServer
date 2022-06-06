66a06a832de57aaa3f4d06e6ab297b9c41ba2991
Claudio Fernandes <ddk.claudio@gmail.com>
2022-06-06 20:22:18 -0300

    add: sinin

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -18,3 +18,22 @@ exports.signUp = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.signIn = async (req, res, next) => {
+  try {
+    const user = await User.get(req.body.email);
+    // https://www.npmjs.com/package/bcrypt
+    if (user && user.password === req.body.password) {
+      // delete user.password;
+      return res.json({ user });
+    } else {
+      error = new Error("Usuario nao encontrado, e-mail ou senha nao conferem");
+      error.status = 200;
+      next(error);
+    }
+  } catch (error) {
+    error = new Error(error);
+    error.status = error.statusCode;
+    next(error);
+  }
+};
